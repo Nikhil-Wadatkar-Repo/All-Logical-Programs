0a3487be0fef2d698ef939d84bbc0905314027c7
nikhil.wadatkar4693@gmail.com <nikhil.wadatkar4693@gmail.com>
2023-02-07 13:49:06 +0530

    interview Programs added

---
+++ src/main/java/com/interview/programs/Test.java
@@ -0,0 +1,10 @@
+package com.interview.programs;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("nikhil");
+
+        System.out.println("ankur");
+    }
+
+}
