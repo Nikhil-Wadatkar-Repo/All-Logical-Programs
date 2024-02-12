748a41fcc6fb8faa889cdd2dcf8e7568b96edda1
Nikhil-Wadatkar-Basket <wadatkarnikhil22@gmail.com>
2024-02-12 16:21:07 +0530

    file modified

--- src/main/java/com/bs/Test.java
+++ src/main/java/com/bs/Test.java
@@ -1,6 +1,6 @@
 package com.bs;
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Nikhil");
+        System.out.println("Interview Preparation");
     }
 }
